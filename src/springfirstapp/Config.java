992edbced39e3b19ed2f22cc2c157d95fea5481f
goldipl <mgodlewski92@gmail.com>
2020-03-29 16:52:07 +0200

    update app

---
+++ src/springfirstapp/Config.java
@@ -0,0 +1,10 @@
+package springfirstapp;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("springfirstapp")
+public class Config {
+
+}
--- src/springfirstapp/MainApp.java
+++ src/springfirstapp/MainApp.java
@@ -1,14 +1,19 @@
 package springfirstapp;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 
-		// load the spring configuration file named 'appxmlfile.xml'
-		ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext("appxmlfile.xml");
+//		load the spring configuration xml file
+//		ClassPathXmlApplicationContext context =
+//				new ClassPathXmlApplicationContext("appxmlfile.xml");
+		
+		// load the spring configuration java file
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(Config.class);
 		
 		Trainer theTrainer = context.getBean("myTrainer", Trainer.class);
 		
